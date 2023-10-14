655218ed53d69249fd136843374a65f531296a68
Twigums <kl305472@gmail.com>
2023-10-14 00:24:16 -0500

    added requirements and added additional functions in video_parser

--- video_parser.py
+++ video_parser.py
@@ -1,16 +1,19 @@
 import os
 import sys
+import random
 import youtube_dl
+import cv2
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import subprocess
 import pytesseract
 from PIL import Image
 
-# returns the path to the video
-def download_video(url, name, output_path):
+# helper function: returns the path to the video after downloading to specified path
+def download_video(url, name, path_output):
     ext = ".mp4"
-    output = output_path + name + ext
+    output = path_output + name + ext
 
+    # params for youtube-dl
     ydl_opts = {
             "outtmpl": output,
             "format": "best[height<=480]/best",
@@ -24,9 +27,13 @@ def download_video(url, name, output_path):
 
     return output
 
-def trim_video(input_video, output_path, start, end):
+# helper function: trim a video from the start to end percentages
+# returns nothing
+def trim_video(input_video, path_output, start, end):
     start = int(start)
     end = int(end)
+
+    # params to find the length of a video since we can't splice through raw percentages
     duration_params = [
             "ffprobe",
             "-v", "error",
@@ -39,16 +46,41 @@ def trim_video(input_video, output_path, start, end):
     start_time = str(duration * start / 100)
     end_time = str(duration * end / 100)
 
+    # after calculating the times, these can be used for the params to trim the video
     params = [
             "ffmpeg", "-i", input_video,
             "-ss", start_time,
             "-to", end_time,
             "-c:v", "copy", "-c:a", "copy",
-            output_path
+            path_output
             ]
 
     subprocess.run(params, check = True)
 
+# helper function: get random input number of frames and save to output path
+def get_frames(path_video, path_output, frames):
+    frames = int(frames)
+
+    video_name = os.path.splitext(os.path.basename(path_video))[0]
+
+    cap = cv2.VideoCapture(path_video)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    frame_indices = random.sample(range(frame_count), frames)
+
+    # go over number of frames
+    for i, frame_index in enumerate(frame_indices):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+        success, frame = cap.read()
+
+        # if frame does exist, then we should write it
+        if success:
+            frame_filename = os.path.join(path_output, f"{video_name}_{i}.jpg")
+            cv2.imwrite(frame_filename, frame)
+
+    cap.release()
+
+# helper function: returns a boolean
 def contains_japanese(text):
 
     # checks if character belongs in the japanese unicode family
@@ -67,7 +99,7 @@ def contains_japanese(text):
 #     print("hellosu")
 # url = "https://www.youtube.com/watch?v=ofSxVONGKTU"
 # name = "test"
-# output_path = "./videos/"
+# path_output = "./videos/"
 
 # returns nothing and saves videos to specified videos folder
 def get_videos():
@@ -78,11 +110,11 @@ def get_videos():
         video_links = file.read().splitlines()
 
         for i, url in enumerate(video_links):
-            output_path = "./videos/"
+            path_output = "./videos/"
 
             if url != "None":
                 try:
-                    output_video = download_video(url, str(i + 1), output_path)
+                    output_video = download_video(url, str(i + 1), path_output)
                 except:
                     print("Could not get video for anime #", i)
 
@@ -92,6 +124,17 @@ def trim_all(start_percentage, end_percentage):
 
     for video in list_videos:
         trim_video(path_to_videos + video, path_to_videos + "trimmed" + video, start_percentage, end_percentage)
+
+def random_frames_all(frames):
+    path_to_videos = "/mnt/b/YouTubeDL/3035-videos/trimmed/"
+    path_to_output = "/mnt/b/YouTubeDL/3035-videos/frames/"
+    list_videos = os.listdir(path_to_videos)
+
+    if not os.path.exists(path_to_output):
+        os.makedirs(path_to_output)
+
+    for video in list_videos:
+        get_frames(path_to_videos + video, path_to_output, frames)
 
 if __name__ == "__main__":
     args = sys.argv
